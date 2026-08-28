91ee04e24d12930d0435ae95b29a8d5d0981a839
bhairavka <chappadigeetha@gmail.com>
2026-08-28 21:06:46 +0530

    Fix source path and update NAT gateway configuration

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc" {
     public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
     enable_nat_gateway = true
-    enable_single_nat_gateway = true
+    single_nat_gateway = true
     enable_dns_hostnames = true
     enable_dns_support = true
     tags = {
@@ -27,7 +27,7 @@ module "vpc" {
 }
 
 module "eks" {
-    source = "terreform-aws-modules/eks/aws"
+    source = "terraform-aws-modules/eks/aws"
     version = "20.8.4"
     cluster_name = var.cluster_name
     cluster_version = var.cluster_version
